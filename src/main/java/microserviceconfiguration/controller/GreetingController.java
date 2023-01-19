d467facfe3ebba693fb63ae60e5a39a7182d5bae
Mithun <mithuncm@gmail.com>
2023-01-19 13:02:27 +0530

    @Value to inject list of values, key-value pairs

--- src/main/java/microserviceconfiguration/controller/GreetingController.java
+++ src/main/java/microserviceconfiguration/controller/GreetingController.java
@@ -1,10 +1,12 @@
 package microserviceconfiguration.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class GreetingController {
@@ -12,8 +14,18 @@ public class GreetingController {
     private String message;
     @Value("${app.description: A default fallback description}")
     private String description;
+
+    @Value("${list.values}")
+    private List<String> listValues;
+
+    /*SPeL: Spring Expression Language*/
+    @Value("#{${db.values}}")
+    private Map<String, String> dbValues;
+
     @GetMapping("/greeting")
     public ResponseEntity<String> greeting() {
-        return ResponseEntity.ok(description);
+        return ResponseEntity.ok(description +
+                "| " + listValues +
+                "| " + dbValues);
     }
 }
