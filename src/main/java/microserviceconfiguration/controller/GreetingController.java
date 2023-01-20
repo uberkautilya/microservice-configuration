6ea8945711986c266cbb83ac0e13d4cd8bb66628
Mithun <mithuncm@gmail.com>
2023-01-20 15:55:03 +0530

    Added spring.application.name to the project. Also annotated the controller with @RefreshScope to enable the configuration refresh of such beans via the actuator/refresh endpoint

--- src/main/java/microserviceconfiguration/controller/GreetingController.java
+++ src/main/java/microserviceconfiguration/controller/GreetingController.java
@@ -3,6 +3,7 @@ package microserviceconfiguration.controller;
 import microserviceconfiguration.configuration.DBSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
-
+/*
+* @RefreshScope tells the spring to refresh the Beans - including its configurations
+* This happens when the appropriate actuator endpoint is called
+* */
 @RestController
+@RefreshScope
 public class GreetingController {
     @Autowired
     Environment environment;
